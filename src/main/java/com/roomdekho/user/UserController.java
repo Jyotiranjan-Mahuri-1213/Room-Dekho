43fcd25c05d41214058285a94456e092aa0776ad
Jyotiranjan-Mahuri-1213 <jyotiranjanmahuri@gmail.com>
2026-06-17 22:17:09 +0530

    User module

--- src/main/java/com/roomdekho/user/UserController.java
+++ src/main/java/com/roomdekho/user/UserController.java
@@ -1,4 +1,26 @@
 package com.roomdekho.user;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
 public class UserController {
+
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+
+        return userService.registerUser(user);
+
+    }
+
 }
